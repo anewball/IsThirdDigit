c3b672fa07a783b41daddb5d683518acf2f34005
anewball <andynewball@gmail.com>
2014-03-27 18:18:24 -0400

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}", IsThirdDigit(1400, 4));
+        }
+
+        public static bool IsThirdDigit(int num, double target)
+        {
+            int num1 = num / 100;
+            double resul = num1 % 10;
+
+            return (resul == target ? true : false);
+        }
+    }
+}
